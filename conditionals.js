90804b5045cbf9e39d83b7469fa55033b642bdb3
abdi osman <abdi_osman254@icloud.com>
2022-08-04 17:03:12 -0500

    my first exercise

---
+++ conditionals.js
@@ -0,0 +1,26 @@
+const cloudy = true;
+const sunny= false;
+
+if (cloudy) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (sunny) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+
+const temperature = 10;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 17) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
