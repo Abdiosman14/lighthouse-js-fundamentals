fbc815c79af981ef4b4854b4cee6283114d8f8ca
abdi osman <abdi_osman254@icloud.com>
2022-08-09 13:51:13 -0500

    additional work from this module

---
+++ how.js
@@ -0,0 +1,14 @@
+let howManyHundreds = 
+function howManyhundreds(number){
+  let x = number
+  let y = number % 100
+  let z = x - y
+  return z / 100
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
+
